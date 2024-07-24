a790ed62939a965f37a96301be78a8313543e97e
Lídia Alves <lisilveir@outlook.com>
2024-07-24 09:52:31 -0300

    Initial Commit

---
+++ AppNotifica-2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica-2024
+//
+//  Created by IFBIOTIC20 on 24/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
